1d58c8940ed1d711ed186e6f3ac387c8e69c19f6
MrBoora <88232848+MrBoora@users.noreply.github.com>
2022-06-10 20:24:27 +0530

    Update Camerafollow.cs

--- Camerafollow.cs
+++ Camerafollow.cs
@@ -7,10 +7,13 @@ public class Camerafollow : MonoBehaviour
     public Transform player;
     public Vector3 offset;
     
+    // "offset" here will decide how far on your camera is placed from the player on each axis
+    // "Transform" will give the position of player to the camera
+    
     void Update()
     {
         transform.position = player.position + offset;
-        // "offset" here will decide how far on your camera is placed from the player on each axis
         
     }
+    // This script is to be added on the camera in your game
 }
