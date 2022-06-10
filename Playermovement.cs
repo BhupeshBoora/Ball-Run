e29a9fad5e2ecb6e8beec708c13ebcf3a48ecef1
MrBoora <88232848+MrBoora@users.noreply.github.com>
2022-06-10 20:29:55 +0530

    Update Playermovement.cs

--- Playermovement.cs
+++ Playermovement.cs
@@ -2,34 +2,31 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Playermovement : MonoBehaviour
-{   // Code for ad movement:
-    Rigidbody rb;
+{  
+
+    Rigidbody rb;              
     public float speed = 10f;
 
     // // // // // //
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
         // Code for forward movement:
-        rb.velocity = 5 * new Vector3(2.5f, 0f, 0f);
+        rb.velocity = 5 * new Vector3(2.5f, 0f, 0f);   
+        //The above line will add a constant velocity to the rigidbody
 
         // // // // // //
 
-        // Code for ad movement:
-        float x = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        // Code for left and right movement:
+        float x = CrossPlatformInputManager.GetAxisRaw("Horizontal");      //To get input from player's keyboard
+        
         Vector3 moveby = transform.right * x * Time.deltaTime;
-
-        rb.MovePosition(transform.position + moveby.normalized * speed * Time.deltaTime);
-
-        // // // // // //
+        rb.MovePosition(transform.position + moveby.normalized * speed * Time.deltaTime);  //To move the rigidbody(rb) on input
 
     }
 
