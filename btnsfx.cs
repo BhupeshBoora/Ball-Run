f779e296776d690b9aa994bcb5495496a0b90ae5
MrBoora <88232848+MrBoora@users.noreply.github.com>
2022-06-11 09:52:15 +0530

    Update btnsfx.cs

--- btnsfx.cs
+++ btnsfx.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class btnsfx : MonoBehaviour
 {
-    public AudioSource myFx;
-    public AudioClip hoverFx;
-    public AudioClip clickFx;
+    public AudioSource myFx;           //The audiosource that's on your player
+    public AudioClip hoverFx;          //Audio that should play when you hover over your button
+    public AudioClip clickFx;          //Audio that should play when you click on your button 
 
+                                       //On your button in Unity, select the "HoverSound" and "ClickSound" functions accordingly 
     public void HoverSound()
     {
-        myFx.PlayOneShot(hoverFx);
+       myFx.PlayOneShot(hoverFx);          
     }
+                                       //"PlayOneShot" makes sure the sound only plays once (each time you hover/click on your button)
     public void ClickSound()
     {
-        myFx.PlayOneShot(clickFx);
+       myFx.PlayOneShot(clickFx);
     }
 
 }
