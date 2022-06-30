136ac4d90fce180789b75cc55fb4f0d10148365a
MrBoora <88232848+MrBoora@users.noreply.github.com>
2022-06-30 21:03:03 +0530

    Update Optionsmenu.cs

--- Optionsmenu.cs
+++ Optionsmenu.cs
@@ -11,43 +11,6 @@ public class Optionsmenu : MonoBehaviour
 
     public AudioMixer audioMixer;
 
-    // Code for Resolution adjustment;
-
-    public TMP_Dropdown resolutionDropdown;
-
-    Resolution[] resolutions;
-
-    void start()
-    {
-        resolutions = Screen.resolutions;
-
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
-
-    public void SetResolution(int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-    }
-    // Code for Volume slider:
-
     public void SetVolume(float Volume)
     {
         audioMixer.SetFloat("Volume", Volume);
